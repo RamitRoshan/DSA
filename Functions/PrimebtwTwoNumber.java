4974659fc85d40d190e2576e928b5f8c088a826d
Ramit Roshann <ramitroshan777777@gmail.com>
2025-08-12 17:09:29 +0530

    modify the functions

--- Functions/PrimebtwTwoNumber.java
+++ Functions/PrimebtwTwoNumber.java
@@ -4,39 +4,35 @@ package Functions;
 //Write a function that returns all prime numbers between two given numbers.
 public class PrimebtwTwoNumber {
 
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter  your number: ");
-//        int x = sc.nextInt();
-//        boolean bo = isPrime(x);
-//        System.out.println(bo);
-//    }
-//    public static boolean isPrime(int n){
-//
-//        if(n<= 1){
-//            return false;
-//        }
-//        for(int i=2; i<=Math.sqrt(n); i++){
-//            if(n %i == 0){
-//                return false;
-//            }
-//        }
-//        return true;
-//    }
-
     public static void main(String[] args) {
-        boolean p = isPrime(3);
-        System.out.println(p);
+         int a = 10, b=14;
+        System.out.print("The prime numbers btw given range is: ");
+         primeRange(a,b);
     }
     public static boolean isPrime(int n){
         if(n<=1){
             return false;
         }
-        for(int i=2; i<=n; i++){
-            if(n % 2 == 0){
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n % i == 0){
                 return false;
             }
         }
         return true;
+
+    }
+    //check prime number btw given ranges
+    public static void primeRange(int a, int b){
+//        for(int i=a; i<=b; i++){
+//            if(isPrime(i)){
+//                System.out.print(i + " ");
+//            }
+//        }
+        //Added Math.min(a, b) and Math.max(a, b) so the range works even if inputs are swapped
+        for (int i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
