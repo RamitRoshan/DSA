4cc825430692854f3df3f2ded4587595f8f97598
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-18 13:27:22 +0530

    solved bubble sort

--- Sorting/Bubble_sort.java
+++ Sorting/Bubble_sort.java
@@ -1,5 +1,26 @@
 package Sorting;
 
 public class Bubble_sort {
+    public static void bubbleSort(int[] array){
+        int n  = array.length;
+        //outer loop
+        for(int i=0; i< n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {7, 6, 1, 4, 3};
+        bubbleSort(arr);
 
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+
+    }
 }
