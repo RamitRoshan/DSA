bbefeb9cb2f7e1a8e2faa80544031803aa4479f2
Ramit Roshann <ramitroshan777777@gmail.com>
2025-09-21 19:45:02 +0530

    add edge cases in linear search algo

--- Searching/LinearSearch.java
+++ Searching/LinearSearch.java
@@ -3,6 +3,10 @@ package Searching;
 public class LinearSearch {
 
     public static int linearSearch(int[] arr, int target){
+
+        //edge cases
+        if(arr.length == 0) return -1;
+
         for(int i=0; i<arr.length; i++){
             if(arr[i] == target){
                 return i;  //return index
