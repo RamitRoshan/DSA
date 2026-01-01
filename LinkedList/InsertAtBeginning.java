584db5cd86cae8375b803ef5b2898b612d2772ca
Ramit Roshann <ramitroshan777777@gmail.com>
2026-01-01 22:48:02 +0530

    updated linkedlist

--- LinkedList/InsertAtBeginning.java
+++ LinkedList/InsertAtBeginning.java
@@ -18,14 +18,23 @@ public class InsertAtBeginning {
 
         //fn to insert at beginning
         void insertAtBeginning(int val){
+
+            //A new Node object is created.
             Node temp = new Node(val);
 
             //empty list
             if(head == null){
-                head = temp;
-                tail = temp;
-            }else{
+                head = temp; //head should point to the new node.
+                tail = temp; //tail should also point to the same node. (Now the list has one node.)
+            }
+            //this block runs when the list already has elements.
+            else{
+                //The new node (temp) points to the current first node.
+                //This connects the new node before the existing list.
+                //before -> head → 14 → 13 → null, after -> temp → 15 → 14 → 13 → null
                 temp.next =head;
+                //Now head is updated to the new node.
+                //This officially makes temp the first node.
                 head = temp;
 
             }
